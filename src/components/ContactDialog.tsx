bcb0644289d8ce956f7bc4de3f13c7e170dbdba5
wappon28dev <wappon_28_dev@ruru.be>
2024-02-27 18:21:56 +0900

    ✨ 確認ダイアログ実装

--- src/components/ContactDialog.tsx
+++ src/components/ContactDialog.tsx
@@ -1,13 +1,50 @@
 import type { ReactElement } from "react";
 import * as AlertDialog from "@radix-ui/react-alert-dialog";
 import { css } from "panda/css";
-import { styled as p } from "panda/jsx";
+import { VStack, styled as p } from "panda/jsx";
+import { useStore } from "@nanostores/react";
+import { $contactFormData } from "@/lib/store/ui";
+import { getEntries } from "@/lib/constants";
+import { formSchema } from "@/lib/services/contact";
+
+const formStyle = css({
+  "& > div": {
+    w: "100%",
+    "& > label": {
+      display: "block",
+      fontWeight: "bold",
+      fontSize: { base: "xl", mdDown: "md" },
+    },
+    "& > input, & > textarea": {
+      w: "100%",
+      h: "12",
+      outline: "1.5px solid",
+      rounded: "md",
+      p: "2",
+      mb: "1",
+      "&:focus": {
+        outlineWidth: "2px",
+      },
+    },
+    "& > textarea": {
+      h: "40",
+      mb: "0",
+    },
+    "& > p": {
+      fontSize: "sm",
+      color: "9u-red1",
+      minH: "5",
+    },
+  },
+});
 
 export function ContactDialog({
   children,
 }: {
   children: ReactElement;
 }): ReactElement {
+  const formData = useStore($contactFormData);
+
   return (
     <AlertDialog.Root>
       <AlertDialog.Trigger asChild>{children}</AlertDialog.Trigger>
@@ -30,11 +67,18 @@ export function ContactDialog({
           className={css({
             position: "fixed",
             top: "50%",
+            overflowY: "auto",
             left: "50%",
             transform: "translate(-50%, -50%)",
             width: "90vw",
-            maxW: "500px",
-            maxH: "85vh",
+            maxW: {
+              base: "60vw",
+              mdDown: "100%",
+            },
+            maxH: {
+              base: "85vh",
+              mdDown: "90vh",
+            },
             p: "5",
             bgColor: "9u-white",
             animation: "fadeIn 0.2s",
@@ -47,17 +91,39 @@ export function ContactDialog({
         >
           <AlertDialog.Title
             className={css({
-              mb: "20px",
               fontWeight: "bold",
               fontSize: "2xl",
               lineHeight: "1.5",
             })}
           >
             送信してもよろしいですか？
+            <p.hr color="9u-brown" />
           </AlertDialog.Title>
           <AlertDialog.Description>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
+            <VStack
+              alignItems="flex-start"
+              className={formStyle}
+              gap="5"
+              py="2"
+            >
+              {getEntries(formSchema).map(
+                ([key, { description, formType, inputType }]) => (
+                  <p.div key={key}>
+                    <p.label htmlFor={key}>{description}</p.label>
+                    {formType === "input" ? (
+                      <p.input
+                        disabled
+                        id={key}
+                        type={inputType}
+                        value={formData[key]}
+                      />
+                    ) : (
+                      <p.textarea disabled id={key} value={formData[key]} />
+                    )}
+                  </p.div>
+                )
+              )}
+            </VStack>
           </AlertDialog.Description>
           <p.div
             className={css({
